9659da02ab8f5f029ebb92c03d0c08a9602da4cf
Yuxuan 'fishy' Wang <wang@yuxuan.org>
2011-08-10 17:07:06 +0800

    * reformat

--- src/com/yhsif/onepwd/OnePwd.java
+++ src/com/yhsif/onepwd/OnePwd.java
@@ -3,13 +3,10 @@ package com.yhsif.onepwd;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class OnePwd extends Activity
-{
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+public class OnePwd extends Activity {
+
+	@Override public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
 }
