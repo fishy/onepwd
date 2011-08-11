f78c9f30147f37cf5ce263836192a75bc47731b9
Yuxuan 'fishy' Wang <wang@yuxuan.org>
2011-08-11 15:49:51 +0800

    - unneccessary comment

--- tests/src/com/yhsif/onepwd/HmacMd5Test.java
+++ tests/src/com/yhsif/onepwd/HmacMd5Test.java
@@ -5,10 +5,6 @@ import junit.framework.TestCase;
 import java.security.NoSuchAlgorithmException;
 
 /**
- * This is a simple framework for a test of an Application.  See
- * {@link android.test.ApplicationTestCase ApplicationTestCase} for more information on
- * how to write and extend Application tests.
- * <p/>
  * To run this test, you can type:
  * adb shell am instrument -w \
  * -e class com.yhsif.onepwd.HmacMd5Test \
