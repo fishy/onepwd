9f742da793573032e64ef9bb687504a9b9c85b4b
Yuxuan 'fishy' Wang <wang@yuxuan.org>
2012-05-01 02:04:30 +0800

    + implemented doGenerate().

--- src/com/yhsif/onepwd/OnePwd.java
+++ src/com/yhsif/onepwd/OnePwd.java
@@ -1,14 +1,19 @@
 package com.yhsif.onepwd;
 
 import android.app.Activity;
-import android.app.Dialog;
-import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class OnePwd extends Activity implements View.OnClickListener {
 
-	private Dialog dialog = null;
+	RadioGroup lengthGroup;
+	TextView master;
+	TextView site;
+	TextView password;
 
 	@Override public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -17,12 +22,18 @@ public class OnePwd extends Activity implements View.OnClickListener {
 		findViewById(R.id.generate).setOnClickListener(this);
 		findViewById(R.id.settings).setOnClickListener(this);
 		findViewById(R.id.close).setOnClickListener(this);
+
+		lengthGroup = (RadioGroup) findViewById(R.id.length_group);
+		master = (TextView) findViewById(R.id.master_key);
+		site = (TextView) findViewById(R.id.site_key);
+		password = (TextView) findViewById(R.id.password);
 	}
 
 	// for View.OnClickListener
 	@Override public void onClick(View v) {
 		switch (v.getId()) {
 			case R.id.generate:
+				doGenerate();
 				break;
 			case R.id.settings:
 				break;
@@ -30,6 +41,26 @@ public class OnePwd extends Activity implements View.OnClickListener {
 				this.finish();
 				break;
 		}
+	}
+
+	public void doGenerate() {
+		RadioButton button = (RadioButton) findViewById(lengthGroup.getCheckedRadioButtonId());
+		int length = Integer.valueOf(button.getText().toString());
+		byte[] array;
+		try {
+			array = HmacMd5.hmac(
+					master.getText().toString(),
+					site.getText().toString().trim());
+		} catch (java.security.NoSuchAlgorithmException e) {
+			// won't happen
+			return;
+		}
+		String value = Base64.encodeToString(array, Base64.URL_SAFE | Base64.NO_PADDING);
+		value = value.replaceAll("\\+", "");
+		value = value.replaceAll("\\/", "");
+		value = value.substring(0, length);
+
+		password.setText(value);
 	}
 
 }
