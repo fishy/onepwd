694f69cf65bdb0b22fdf56c987bbe45a906f54f2
Yuxuan 'fishy' Wang <wang@yuxuan.org>
2024-07-04 11:09:49 -0700

    Bump version to 1.9.0

--- WORKSPACE
+++ WORKSPACE
@@ -2,7 +2,7 @@ workspace(name = "onepwd")
 
 android_sdk_repository(
     name = "androidsdk",
-    api_level = 33,
+    api_level = 34,
 )
 
 load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive", "http_jar")
--- src/com/yhsif/onepwd/QSTileService.kt
+++ src/com/yhsif/onepwd/QSTileService.kt
@@ -2,13 +2,24 @@ package com.yhsif.onepwd
 
 import android.content.Intent
 import android.service.quicksettings.TileService
+import androidx.core.service.quicksettings.PendingIntentActivityWrapper
+import androidx.core.service.quicksettings.TileServiceCompat
 
 class QSTileService : TileService() {
   override fun onClick() {
     unlockAndRun() {
       val intent = Intent(this, OnePwd::class.java)
       intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-      startActivityAndCollapse(intent)
+      TileServiceCompat.startActivityAndCollapse(
+        this,
+        PendingIntentActivityWrapper(
+          this,
+          0, // requestCode
+          intent,
+          Intent.FLAG_ACTIVITY_NEW_TASK,
+          false, // isMutable
+        ),
+      )
     }
   }
 }
